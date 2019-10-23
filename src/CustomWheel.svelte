f4044905f308cff111145803828307896ce95f32
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-10-23 22:23:00 +0600

    fix

--- src/CustomWheel.svelte
+++ src/CustomWheel.svelte
@@ -3,11 +3,11 @@
   import { afterUpdate } from 'svelte';
   
   export let selected = 0;
-  let position = selected ? -(selected - 1) * 50 : 0 }
-  let offset = 0
+  let position = selected ? -(selected - 1) * 50 : 0;
+  let offset = 0;
   let dragging = false;
 
-  let itemWrapper;
+  let itemWrapper, previousY;
 
   let itemPosition = `
       will-change: 'transform';
