d52ebdf183595fb70396e6408b9f016f20c8429c
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-10-23 20:57:04 +0600

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import svelte from 'rollup-plugin-svelte';
+import bundleSize from 'rollup-plugin-bundle-size'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+export default [
+	{
+		input: 'src/Swipe.svelte',
+		output: [
+			{ file: pkg.module, 'format': 'es' },
+			{ file: pkg.main, 'format': 'umd', name: 'DatePicker' }
+		],
+		plugins: [
+			svelte(),
+			commonjs(),
+      		resolve(),
+      		terser(),
+      		bundleSize()
+		]
+	}
+];
