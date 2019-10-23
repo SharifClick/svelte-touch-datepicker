06631f4abc13ed4990df14bbf5994a9e9b115042
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-10-23 21:02:04 +0600

    style reset

--- dev/App.svelte
+++ dev/App.svelte
@@ -9,6 +9,19 @@
     height: 100%;
     width: 100%;
   }
+
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    font-size: 12px;
+  }
   
 </style>
 
