1385c384b8cde3c6fd58409d194e5409f0d2ece4
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-10-23 20:57:14 +0600

    entry file

---
+++ dev/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { DatePicker } from "../src/index.js";
+ 
+
+</script>
+
+<style>
+  .container, .is-stack{
+    height: 100%;
+    width: 100%;
+  }
+  
+</style>
+
+<div class="container" >
+  <DatePicker />
+</div>
+
