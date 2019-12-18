6e438e741c4f85560c172e020979ec04a911eed9
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-12-18 21:24:33 +0600

    doc changes

--- dev/App.svelte
+++ dev/App.svelte
@@ -1,29 +1,16 @@
 <script>
   import { afterUpdate} from 'svelte';
-  import {DatePicker, TimePicker}  from "../src/index.js";
+  import {DatePicker}  from "../src/index.js";
 
-  export let mode = 'time';
 
   let date = new Date();
-  let time = new Date();
   let visible = false;
-  let visibleTime = false;
-  let inputDate;
-
-  afterUpdate(() => {
-	  
-  });
 
   function toggle(){
     visible = !visible
   }
-   function toggleTime(){
-    visibleTime = !visibleTime
-  }
 
   $: _date = date.toLocaleDateString("en-US");
-  // $: _time = time.toLocaleTimeString("en-US");
-  $: _inputdate = new Date(inputDate);
 
 
 
@@ -50,11 +37,6 @@
   <div class="center">
     <p on:click={toggle}>{_date}</p>
     <DatePicker bind:date bind:visible/>
-
-    <hr>
-    <p on:click={toggle}>{time}</p>
-    
-    <TimePicker bind:time/>
   </div>
 </div>
 
